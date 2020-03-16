783efa551eda391fbb6ff14a929c2516e56fc814
Christian <pixman92@gmail.com>
2020-03-15 23:05:45 -0700

    Adding Saved Emails - working, HTML, JS still not there

---
+++ .history/2_0/savedEmails_20200315225923.js
@@ -0,0 +1,17 @@
+
+var savedEmailsArr=[];
+async function savedEmails(email){
+    try{
+        var one1 = await one();
+        var two2 = await two(one1);
+        // var three3 = await three(two2, myStatus);
+
+    }catch(e){
+        console.log('e', e);
+        throw e;
+    }
+
+    async function one(){
+        document.getElementById('dynamicSavedEmails').innerHTML = 
+
+}
---
+++ .history/2_0/savedEmails_20200315230158.js
@@ -0,0 +1,29 @@
+
+var savedEmailsArr=[];
+async function savedEmails(email){
+    try{
+        var one1 = await one();
+        var two2 = await two(one1);
+        // var three3 = await three(two2, myStatus);
+
+    }catch(e){
+        console.log('e', e);
+        throw e;
+    }
+
+    async function one(){
+        document.getElementById('dynamicSavedEmails').innerHTML = 
+        savedEmailsArr.push('<div class="gridSavedEmailAndX">');
+        savedEmailsArr.push('<div>'
+
+        savedEmailsArr.push('<li id="hello">hello</li>'
+
+        savedEmailsArr.push(</div>
+        savedEmailsArr.push(<div>
+        savedEmailsArr.push(                                <button class="w3-button w3-blue smfont">X</button>
+
+        savedEmailsArr.push(</div>
+        savedEmailsArr.push(</div>
+        savedEmailsArr.push(</div>
+
+}
