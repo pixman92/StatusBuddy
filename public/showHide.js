2fe0c9c440603873adf967a69b19b95cb9a4a7bb
Christian B <christianbear1221@gmail.com>
2020-03-28 23:09:49 -0700

    Working? - made the search page to include time stamp! need to adjust the hours, though.

--- public/showHide.js
+++
@@ -1,9 +0,0 @@
-//functions to show/hide divs
-function hide(id){
-    document.getElementById(id).classList.add("hidden");
-    
-}
-function show(id){
-    document.getElementById(id).classList.remove("hidden");
-
-}
